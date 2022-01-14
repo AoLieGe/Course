981877d0479d1646c83ec35bd7a2c07c8ed18f8c
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2022-01-14 23:53:47 +0500

    update data
    add set_funds method which set funds from incoming dict

--- data.py
+++ data.py
@@ -2,3 +2,9 @@ class CurrencyData:
     def __init__(self, currency_list):
         self.funds = {cur: 0 for cur in currency_list}
         self.course = {cur: 0 for cur in currency_list}
+
+    def set_funds(self, currency_funds):
+        # method set funds for all currencies in currency_funds dict if it exist in self.funds
+        for cur, funds in currency_funds.items():
+            if cur in self.funds.keys():
+                self.funds[cur] = funds
