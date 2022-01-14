663f9c2844fe5a8a48308b0a9bea0fa3b13867bb
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2022-01-14 22:44:18 +0500

    arguments updated
    rename method and add all currency arguments

--- arguments.py
+++ arguments.py
@@ -1,12 +1,11 @@
-import argparse
-
-
-def get():
-    parser = argparse.ArgumentParser(description="Currency course script")
+def set_script_args(parser, currencies):
+    # method add base app arguments: period and debug
+    # and add all currency values arguments
     parser.add_argument("-period", dest="period", type=int, required=True,
                         help='Period of course request (minutes)')
     parser.add_argument("-debug", dest="debug", type=str, default='0',
                         help='set this to output debug info to console')
-    parser.print_help()
 
-    return parser.parse_args()
+    currencies_set = set(currencies)
+    for cur in currencies_set:
+        parser.add_argument(f'-{cur}', dest=f"{cur}", type=float)
