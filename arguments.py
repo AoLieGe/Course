21b52ae8c0c2d808ab314b1a6b153323abb96bf7
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2022-01-16 13:22:36 +0500

    Update arguments.py
    create class AppArguments
    func set_script_args is class method now

--- arguments.py
+++ arguments.py
@@ -1,11 +1,27 @@
-def set_script_args(parser, currencies):
-    # method add base app arguments: period and debug
-    # and add all currency values arguments
-    parser.add_argument("-period", dest="period", type=int, required=True,
-                        help='Period of course request (minutes)')
-    parser.add_argument("-debug", dest="debug", type=str, default='0',
-                        help='set this to output debug info to console')
+import argparse
 
-    currencies_set = set(currencies)
-    for cur in currencies_set:
-        parser.add_argument(f'-{cur}', dest=f"{cur}", type=float)
+
+class AppArguments:
+    def __init__(self, currencies: list):
+        self.parser = argparse.ArgumentParser(description="Currency course script")
+        self._set_script_args(currencies)
+
+    def read(self) -> dict:
+        """return dict with command line arguments and values"""
+        return vars(self.parser.parse_args())
+
+    def _set_script_args(self, currencies: list) -> None:
+        """set application arguments
+            -period X       X (int) - period (min) of course requests
+            -debug X        X (str) - if value = (1, true, True, y, Y), set debug logging level
+            -currency_name  X (float) - get currency starting funds (for each in currencies list)
+        """
+
+        self.parser.add_argument("-period", dest="period", type=int, required=True,
+                                 help='Period of course request (minutes)')
+        self.parser.add_argument("-debug", dest="debug", type=str, default='0',
+                                 help='set this to output debug info to console')
+
+        currencies_set = set(currencies)
+        for cur in currencies_set:
+            self.parser.add_argument(f'-{cur}', dest=f"{cur}", type=float)
