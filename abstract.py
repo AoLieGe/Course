3700fd9fe0ed4f770e83dcd02e69c6fab56afa9e
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2022-01-16 13:17:36 +0500

    update abstract
    rename class
    listener method replaced by group of server route methods
    rename provider to provide_course
    rename informer to inform

--- abstract.py
+++ abstract.py
@@ -1,19 +1,35 @@
+from aiohttp import web
 from abc import ABC, abstractmethod
 
 
-class AbstractCourse(ABC):
+class AbstractServer(ABC):
     @abstractmethod
-    async def listener(self):
-        # method listen external requests from http port
+    async def get_amount(self, request: web.Request) -> web.Response:
+        """route method - response with all course data"""
         pass
 
     @abstractmethod
-    async def provider(self, delay):
-        # method request actual course from external URL
+    async def set_amount(self, request: web.Request) -> web.Response:
+        """route method - set currency funds"""
         pass
 
     @abstractmethod
-    async def informer(self, delay):
-        # method show current actual data in console
+    async def modify(self, request: web.Request) -> web.Response:
+        """route method - modify currency funds"""
+        pass
+
+    @abstractmethod
+    async def get_currency(self, request: web.Request) -> web.Response:
+        """route method - get currency course value"""
+        pass
+
+    @abstractmethod
+    async def provide_course(self) -> None:
+        """get json data from external URL and save it"""
+        pass
+
+    @abstractmethod
+    async def inform(self) -> None:
+        """show current actual data in console if they have updates"""
         pass
 
