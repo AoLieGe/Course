a906dcb95dd193fbaaaebee3dc95d835f7904c71
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2022-01-16 13:29:26 +0500

    Update main.py
    full refactoring

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import argparse
-import logging
-import asyncio
-from arguments import set_script_args
-from impl import Course
-from data import CurrencyData
-
-
-def set_debug(param):
-    if param in [1, 'true', 'True', 'y', 'Y']:
-        logging.basicConfig(level=logging.DEBUG)
+from arguments import AppArguments
+from debug import set_debug_level
+from model import CurrencyData
+from impl import CourseServer
 
 
 if __name__ == '__main__':
-    print("Cource application started!")
+    print("Message:  Course application started!\n")
     currency_list = ['USD', 'EUR', 'RUB']
+
+    # init data model
     currency_data = CurrencyData(currency_list)
 
-    parser = argparse.ArgumentParser(description="Currency course script")  # create app arg parser
-    set_script_args(parser, currency_list)  # set app arguments
-    args = parser.parse_args()  # parse arguments
+    # read app cmd line arguments to dict
+    args = AppArguments(currency_list).read()
 
-    args_dict = vars(args)  # convert arguments to dict
-    set_debug(args_dict['debug'])  # set debug status from arguments
+    # extract values from parameters
+    debug_status = args['debug']
+    provide_delay = args['period']
+    currency_funds = {c: f for c, f in args.items() if c in currency_list}
 
-    currency_funds = {c: f for c, f in args_dict.items() if c in currency_list}  # get dict of currency funds
+    # set values
+    set_debug_level(debug_status)
     currency_data.set_funds(currency_funds)
 
-    course = Course(currency_data)
-
-    # testing of course requests
-    asyncio.get_event_loop().run_until_complete(asyncio.gather(course.provider(6), course.informer(10)))
-
+    # create server instance and start it
+    server = CourseServer(currency_data, provide_delay)
+    server.start('localhost', 8080)
